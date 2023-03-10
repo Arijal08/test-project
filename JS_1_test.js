4a261b0935bf83864e81af419d582e44931c9d44
unknown <arijalsidik1606@gmail.com>
2023-03-10 18:09:57 +0700

    initial commit

---
+++ JS_1_test.js
@@ -0,0 +1,18 @@
+// Contoh ini tidak bisa di run disini
+import { isPrime, random } from "./utils";
+
+export function generatesPrimes(maximum) {
+  const MAX_PRIME = 1000000;
+    
+  const primes = [];
+    
+  while (primes.length < maximum) {
+    const candidate = random(MAX_PRIME);
+    
+    if (isPrime(candidate)) {
+      primes.push(candidate);
+    }
+  }
+    
+  return primes;
+}
---
+++ JS_2_setTimeOut.js
@@ -0,0 +1,10 @@
+/* setTimeOut(), adalah fungsi di JavaScript yang digunakan untuk menjadwalkan pemanggilan sebuah kode 
+sekaligus membuatnya berjalan secara asynchronous */
+
+console.log('Selamat Datang');
+
+setTimeout(()=>{
+    console.log('Nama saya adalah Arijal Sidik');
+}, 3000)
+
+console.log('Saya lahir di situjuah')
